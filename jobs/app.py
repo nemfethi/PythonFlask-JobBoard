bc6bc4a41f1ea09aaf3af0fd23325b3d52813d64
Fethi Nemir <fethi.nemir.ext@gfi.fr>
2020-02-09 11:25:56 +0100

    cursor exec

--- jobs/app.py
+++ jobs/app.py
@@ -18,6 +18,8 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results=connection.commit
     else:
         results=cursor.fetchone() if single else cursor.fetchall()
+    
+    cursor.close()
     return results
 
 @app.route('/')
